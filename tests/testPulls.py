f33516b6c5b7b43421dab4d42827353ffcd4693b
pambalos <bradleyjusticeca@gmail.com>
2018-08-02 20:47:40 -0700

    Updates

--- tests/testPulls.py
+++ tests/testPulls.py
@@ -1,9 +1,13 @@
 from keys import *
-import requests
+import requests, json
 
-params = {'client_id': 'a110738a6c9248638dcdf8be283d3bcb', 'response_type': 'code', 'scope': 'check', 'redirect_uri': callback}
+params = {'client_id': cid, 'response_type': 'code', 'scope': 'check', 'redirect_uri': callback}
 
-response = requests.request("POST", 'https://checkbook.io/oauth/authorize'
+response = requests.request("GET", 'https://checkbook.io/oauth/authorize'
 , headers = params)
+print('Attempt using requests results in:')
+print(response.text)
 
-print(response)
+#attempt two
+heads_test = {"Content-Type" : "application/json", "X-Accept" : "application/json"}
+payload = {"client_id" : cid, "redirect_uri" : callback}
