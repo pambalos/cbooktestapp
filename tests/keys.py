ddd9b3582e15ad721585c77996db1d61b8bc289e
pambalos <bradleyjusticeca@gmail.com>
2018-08-03 23:25:59 -0700

    Updated

--- tests/keys.py
+++ tests/keys.py
@@ -5,3 +5,4 @@ urlB = "https://demo.checkbook.io/v3/check"
 apikey = 'e3c43713fd6746f8836bd9361ac0b954'
 apisecret = 'QwqvFVhhyRqjBl2zej4V5pU60HoOWf'
 webhhook = '9c42446d969446bdb0f796e9bd355301'
+urlToken = 'https://checkbook.io/oauth/token'
--- tests/testPulls.py
+++ tests/testPulls.py
@@ -4,14 +4,24 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
+from flask_oauthlib.client import Oauth
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] =' 67aGHYDS8c7S8CGcaydw878csa7887bac' #setting secret key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-params = {'client_id': cid, 'response_type': 'code', 'scope': 'check', 'redirect_uri': callback}
+params = {
+    'client_id': cid,
+    'response_type': 'code',
+    'scope': 'check',
+    'redirect_uri': callback
+}
 
-response = requests.request("POST", 'https://checkbook.io/oauth/authorize', headers = params)
+client = BackendApplicationClient(client_id = cid)
+oauth = requests.Session(client_id = client)
+token = oauth.fetch_token(token_url = urlToken, client_id = cid, client_secret = apisecret)
+
+response = requests.request("GET", 'https://checkbook.io/oauth/authorize', headers = params)
 print('Attempt using requests results in:')
 print(response)
 
@@ -28,6 +38,7 @@ def home():
     form = testForm()
     if form.validate_on_submit():
         flash('Submitted', 'success')
+        return redirect();
     return render_template('layout.html', title = 'Test', form = form)
 
 if __name__ == '__main__':
