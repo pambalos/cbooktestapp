d4dc785d63173f3afd2ee9c55191345b1a945d08
pambalos <bradleyjusticeca@gmail.com>
2018-08-04 19:01:30 -0700

    Updates

--- tests/testPulls.py
+++ tests/testPulls.py
@@ -8,7 +8,7 @@ from flask_oauthlib.client import OAuth
 from requests_oauthlib import OAuth2Session
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] =' 67aGHYDS8c7S8CGcaydw878csa7887bac' #setting secret key
+app.config['SECRET_KEY'] ='67aGHYDS8c7S8CGcaydw878csa7887bac' #setting secret key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 params = {
@@ -17,6 +17,17 @@ params = {
     'scope': 'check',
     'redirect_uri': callback
 }
+
+#Authorization through user-agent, currently sends out right, not sure about response
+oauth = OAuth2Session(cid, redirect_uri = callback, scope = 'check')
+authorization_url, state = oauth.authorization_url(req_url)
+response_oauth = requests.request("GET", authorization_url, headers = params)
+print ('Authorization_url: %s' % authorization_url)
+print('response_oauth is given as: %s' % response_oauth)
+print(response_oauth.text)
+# auth_resp = input(callback)
+# print(auth_resp)
+
 
 #attempt two
 heads_test = {"Content-Type" : "application/json", "X-Accept" : "application/json"}
@@ -30,25 +41,16 @@ class testForm(FlaskForm):
 def home():
     form = testForm()
     if form.validate_on_submit():
+        print('validate_on_submit entered')
         flash('Submitted', 'success')
-        return redirect();
+        return redirect(authorization_url, code = 302);
     return render_template('layout.html', title = 'Test', form = form)
 
-#Authorization through user-agent, currently sends out right, not sure about response
-oauth = OAuth2Session(cid, redirect_uri = callback, scope = 'check')
-authorization_url, state = oauth.authorization_url(req_url)
-response_oauth = requests.request("GET", authorization_url, headers = params)
-print ('Authorization_url: %s' % authorization_url)
-print('response_oauth is given as: %s' % response_oauth)
-print(response_oauth.text)
-# auth_resp = input(callback)
-# print(auth_resp)
-
-
+"""
 response = requests.request("GET", 'https://checkbook.io/oauth/authorize', headers = params)
 print('Attempt using requests results in:')
 print(response)
-
+"""
 
 if __name__ == '__main__':
     app.run(debug=True)
